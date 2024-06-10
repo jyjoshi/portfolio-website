0930aff6ec149c6c3a722c478bfab8d37493515e
Jay Joshi <jayjoshi112711@gmail.com>
2024-06-10 00:35:32 -0700

    No roots please

--- pages/index.js
+++ pages/index.js
@@ -36,12 +36,12 @@ export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [activePDF, setActivePDF] = useState("");
-  const root_url = "https://jyjoshi.github.io/portfolio-website/";
+  // const root_url = "https://jyjoshi.github.io/portfolio-website/";
   // const root_url = "localhost:3000/";
 
   const handlePDFOpen = (pdfPath) => {
     setDropdownOpen(false);
-    setActivePDF(root_url + pdfPath);
+    setActivePDF(pdfPath);
   };
 
   const handleClose = () => {
